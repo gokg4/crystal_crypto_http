a9fadddbcc88dab59ba0d314a7a36917e46e19d0
Gokul <gokulg4@gmail.com>
2022-05-12 11:02:11 +0530

    Update01 on 12-05-2022

--- src/http_crypto_price.cr
+++ src/http_crypto_price.cr
@@ -10,24 +10,32 @@ module HttpCryptoPrice
   puts "which crypto price do you want to check? eg: etheruem,bitcoin,tether,usd-coin...etc"
   print "> "
   crypto = gets
-  exit if crypto.nil?
+  case
+  when crypto == ""
+    exit
+    puts "You have not given an input. Check value of Bitcoin"
+    crypto = "bitcoin"
+  else
+    puts "Please wait...."
+    crypto = crypto.to_s.downcase
+  end
   date = Time.local
   uri_Host = URI.parse "api.coingecko.com"
   uri_Path = URI.parse "/api/v3/simple/price"
-  params = URI::Params.encode({"ids" => crypto, "vs_currencies" => "inr,usd,eur,gbp"}) # => "author=John+Doe&offset=20"
+  params = URI::Params.encode({"ids" => crypto, "vs_currencies" => ["inr,usd,eur,gbp"]})
   response = HTTP::Client.get URI.new("https", uri_Host.to_s, query: params, path: uri_Path.to_s)
   if response.status_code == 200
     json_response = JSON.parse(response.body)
-    inr = json_response[crypto.downcase]["inr"].to_s
+    inr = json_response[crypto]["inr"].to_s
     inr_str = inr.to_f
     inr_fr = Money.new(inr_str, "INR").format
-    usd = json_response[crypto.downcase]["usd"].to_s
+    usd = json_response[crypto]["usd"].to_s
     usd_str = usd.to_f
     usd_fr = Money.new(usd_str, "USD").format
-    eur = json_response[crypto.downcase]["eur"].to_s
+    eur = json_response[crypto]["eur"].to_s
     eur_str = eur.to_f
     eur_fr = Money.new(eur_str, "EUR").format
-    gbp = json_response[crypto.downcase]["gbp"].to_s
+    gbp = json_response[crypto]["gbp"].to_s
     gbp_str = gbp.to_f
     gbp_fr = Money.new(gbp_str, "GBP").format
     puts "\n"
